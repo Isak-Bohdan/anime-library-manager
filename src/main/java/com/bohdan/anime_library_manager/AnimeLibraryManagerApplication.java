6e2d1ebe402f57f8570e5dd562ed24645bf2162b
Isak Bohdan <isakbohdan@gmail.com>
2026-03-27 19:49:04 +0200

    A Spring Boot project has been created, and a connection to the database has been established <3

---
+++ src/main/java/com/bohdan/anime_library_manager/AnimeLibraryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bohdan.anime_library_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLibraryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeLibraryManagerApplication.class, args);
+	}
+
+}
